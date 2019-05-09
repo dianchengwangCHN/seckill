cff4352c2b895316f8ab7a7a640ca4984a4491f3
dianchengwangCHN <dianchengwangchn@gmail.com>
2019-05-08 23:43:42 -0400

    build dao tier

---
+++ server/src/main/java/com/seckill/seckill/dao/GoodsDao.java
@@ -0,0 +1,8 @@
+package com.seckill.seckill.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface GoodsDao {
+
+}
---
+++ server/src/main/java/com/seckill/seckill/dao/OrderDao.java
@@ -0,0 +1,26 @@
+package com.seckill.seckill.dao;
+
+import com.seckill.seckill.domain.OrderInfo;
+import com.seckill.seckill.domain.SeckillOrder;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface OrderDao {
+
+    @Select("select * from seckill_order where user_id=#{userId} and goods_id=#{goodsId}")
+    public SeckillOrder getSeckillOrderByUserIdGoodsId(@Param("userId")long userId, @Param("goodsId")long goodsId);
+
+    @Insert("insert into order_info (user_id, goods_id, goods_name, goods_count, goods_price, order_channel, status," +
+            "create _date)values(#{userId}, #{goodsId}, #{goodsName}, #{goodsCount}, #{goodsPrice}, #{orderChannel}, " +
+            "#{status}, #{createDate})")
+    public long insert(OrderInfo orderInfo);
+
+    @Insert("insert into seckill_order (user_id, goods_id, order_id)values(#{userId}")
+    public int insertSeckillOrder(SeckillOrder seckillOrder);
+
+    @Delete("delete from order_info")
+    public void deleteOrders();
+
+    @Delete("delete from seckill_order")
+    public void deleteSeckillOrders();
+}
