874340b69d3262e577738a6366c6cd1e9e1aa527
dianchengwangCHN <dianchengwangchn@gmail.com>
2019-05-19 20:12:18 -0400

    refactor

---
+++ server/src/main/java/com/seckill/config/UserArgumentResolver.java
@@ -0,0 +1,31 @@
+package com.seckill.config;
+
+import com.seckill.access.UserContext;
+import com.seckill.domain.SeckillUser;
+import com.seckill.service.SeckillUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Service
+public class UserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Autowired
+    private SeckillUserService userService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        Class<?> clazz = parameter.getParameterType();
+        return clazz == SeckillUser.class;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavController,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return UserContext.getUser();
+    }
+}
