0a76fd442b45086ee173dcf6756e4d88ab981798
dianchengwangCHN <dianchengwangchn@gmail.com>
2019-05-20 22:24:56 -0400

    fix some bugs

--- server/src/main/java/com/seckill/config/WebConfig.java
+++ server/src/main/java/com/seckill/config/WebConfig.java
@@ -3,14 +3,25 @@ package com.seckill.config;
 import com.seckill.access.AccessInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
+    private UserArgumentResolver userArgumentResolver;
+
+    @Autowired
     private AccessInterceptor accessInterceptor;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(userArgumentResolver);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
